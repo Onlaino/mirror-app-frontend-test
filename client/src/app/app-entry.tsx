a8021560fe35d348b6dbf38a17af7d097ecd233a
Oleg Vasilev <v.oleg2908@gmail.com>
2024-09-25 14:10:58 +0300

    done some todos

--- client/src/app/app-entry.tsx
+++ client/src/app/app-entry.tsx
@@ -4,10 +4,10 @@ import { PostPage } from '@/pages/post-page';
 import { LayoutContextProvider } from '@/entities/context/LayoutContext';
 
 // TODO:  Приложение не должно ломаться при добавлении/удалении нового layout или template. Добавлять или удалять layout, как и template должно быть легко и с минимальным изменением текущего функционала и кодовой базы.
-// TODO: переписать useFetch на axios
-// TODO: уточнить, как должны себя вести rows
+// TODO: как должны себя вести rows
 
-function App() {
+
+const App = () => {
 	return (
 		<main className='px-44 min-h-screen'>
 			<LayoutContextProvider>
